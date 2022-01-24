58f73bf93e98d7af160de82f2d58a12c31444926
Ken Power <github@kenpower.com>
2022-01-24 16:56:23 +0000

    4th failing

--- FixzzBuzz-Test/test.cpp
+++ FixzzBuzz-Test/test.cpp
@@ -16,3 +16,8 @@ TEST(FizzBuzz, Returns_1_AsString) {
 	std::string result = fizzBuzz(1);
 	EXPECT_EQ("1", result); // test fails if the two parameters are not equal 
 }
+
+TEST(FizzBuzz, Returns_2_AsString) {
+	std::string result = fizzBuzz(1);
+	EXPECT_EQ("2", result); // test fails if the two parameters are not equal 
+}
